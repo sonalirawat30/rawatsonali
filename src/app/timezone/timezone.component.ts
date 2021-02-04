0a0905cd68b38c6b6074fe97d77c2f66c03cd5a5
sonali rawat <sonalirawat3003@gmail.com>
2021-02-04 13:02:46 -0700

    timezone service

--- src/app/timezone/timezone.component.ts
+++ src/app/timezone/timezone.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment-timezone';
+import { TimeZoneService } from '../timezone.service';
+// import * as moment from 'moment-timezone';
 
 @Component({
 selector: 'app-timezone',
@@ -7,22 +8,37 @@ templateUrl: './timezone.component.html',
 styleUrls: ['./timezone.component.css']
 })
 export class TimezoneComponent implements OnInit {
-time: any;
+// time: any;
+dataOfZone:any;
 todayDate = { name: 'India Standard Time', date: 'short', timeZone: new Date() };
 
-constructor() { }
+constructor(private timeZone:TimeZoneService) { }
 
 ngOnInit(): void {
-this.time = [
-{ name: 'India Standard Time', date: 'full', timeZone: new Date() },
-{ name: 'China Standard Time', date: 'short', timeZone: new Date()},
-{ name: 'us Time', date: 'short', timeZone: new Date() },
-{ name: 'uk Standard Time', date: 'short', timeZone: new Date() }
-]
+// this.time = [
+// { name: 'India Standard Time', date: 'full', timeZone: new Date() },
+// { name: 'China Standard Time', date: 'short', timeZone: new Date()},
+// { name: 'us Time', date: 'short', timeZone: new Date() },
+// { name: 'uk Standard Time', date: 'short', timeZone: new Date() }
+// ]
+// }
+
+// public selectedValue(item){
+//   this.todayDate = this.time.find((val)=>val.name === item);
+//   }
+// }
+this.dataOfZone = this.timeZone.GetTimeZone();
 }
 
-public selectedValue(item){
-  this.todayDate = this.time.find((val)=>val.name === item);
-  }
-}
+ changeTimezone() { 
+      
+  let date =  new Date() 
+  console.log('Given IST datetime: ' + date); 
 
+  let usaTime =  
+      date.toLocaleString("en-US", { 
+          timeZone: "_timeZone"  
+      }); 
+  console.log('USA datetime: ' + usaTime); 
+} 
+}
